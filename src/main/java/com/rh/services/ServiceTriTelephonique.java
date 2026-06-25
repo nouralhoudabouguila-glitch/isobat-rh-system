f5195f3787ec8e4c0a8a7cc093b3b96306eb5c0b
meriem-bgl <meriem.bouguila@icloud.com>
2026-06-25 11:07:43 +0100

    test

--- src/main/java/com/rh/services/ServiceTriTelephonique.java
+++ src/main/java/com/rh/services/ServiceTriTelephonique.java
@@ -82,7 +82,7 @@ public class ServiceTriTelephonique implements IServices<TriTelephonique> {
             e.printStackTrace();
         }
 
-        return candidat
+        return candidats;
     }
 
     @Override
